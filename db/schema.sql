d4a79cf0c1f1afc02c168a620f9dfb181da5710d
Mikayla Bruce <mkla.w.b@gmail.com>
2022-01-30 18:22:33 -0800

    keys commpleted for sql files and auto increment added for the id values

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,32 @@
 DROP DATABASE IF EXISTS employees_db;
-
 CREATE DATABASE employees_db;
-
 USE employees_db;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    dept_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (dept_id)
 );
 
-CREATE TABLE role (
-    id INT PRIMARY KEY,
+CREATE TABLE role(
+    role_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL
+    dept_id INT,
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (dept_id)
+    REFERENCES department(dept_id)
     );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY NOT NULL,
+    employ_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
-);
+    employ_role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (employ_id),
+    FOREIGN KEY (employ_role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(employ_id)
+    );
 
 
